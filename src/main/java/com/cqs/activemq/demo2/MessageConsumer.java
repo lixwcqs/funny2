e7326a43fef23c12a938867265c034e424a904b5
cloud <congj.yang@hotmail.com>
2016-11-05 11:11:14 +0800

    练习项目 J2EE

---
+++ src/main/java/com/cqs/activemq/demo2/MessageConsumer.java
@@ -0,0 +1,48 @@
+package com.cqs.activemq.demo2;
+
+import org.apache.activemq.*;
+
+import javax.jms.*;
+import javax.jms.Message;
+
+/**
+ * Created by cloud on 16-11-5.
+ */
+public class MessageConsumer implements MessageListener{
+
+
+
+    public void work() throws JMSException {
+        Connection connection = ActiveMQUtils.getConnectFactory().createConnection();
+        connection.start();
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("f1");
+        javax.jms.MessageConsumer consumer = session.createConsumer(queue);
+
+        Message message = consumer.receive(10000);
+        System.out.println(message);
+
+        session.close();
+        connection.close();
+    }
+
+
+    public void onMessage(Message message) {
+    }
+
+    public static void main(String[] args) throws JMSException {
+        new MessageConsumer().work();
+    }
+}
+
+class ActiveMQUtils{
+    public final static String USER = "admin";
+    public final static String PASSWORD = "admin";
+    public final static String URL = "tcp://localhost:61616";
+    public final static ConnectionFactory factory =  new ActiveMQConnectionFactory(USER,PASSWORD,URL);
+
+    public static ConnectionFactory getConnectFactory(){
+        return factory;
+    }
+
+}
---
+++ src/main/java/com/cqs/activemq/demo2/MessageProducer.java
@@ -0,0 +1,29 @@
+package com.cqs.activemq.demo2;
+
+
+import org.apache.activemq.*;
+import javax.jms.*;
+
+/**
+ * Created by cloud on 16-11-5.
+ */
+public class MessageProducer {
+
+    public void work() throws JMSException {
+        ConnectionFactory factory = ActiveMQUtils.getConnectFactory();
+        Connection connection = factory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("f1");
+        javax.jms.MessageProducer producer = session.createProducer(queue);
+        producer.send(session.createTextMessage("Hello world,Activemq"));
+
+        session.close();
+        connection.close();
+
+    }
+
+
+    public static void main(String[] args) throws JMSException {
+        new MessageProducer().work();
+    }
+}
