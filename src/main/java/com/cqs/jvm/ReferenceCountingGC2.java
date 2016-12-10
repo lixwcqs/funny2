881d4bd6b127baf321c39c33bb2ea43839092dd4
lixwcqs <lixwcqs@163.com>
2016-12-10 23:24:17 +0800

    gc

---
+++ src/main/java/com/cqs/jvm/ReferenceCountingGC2.java
@@ -0,0 +1,46 @@
+package com.cqs.jvm;
+
+/**
+ * -XX:+PrintGCDetails
+ * -XX:+PrintGCDateStamps
+ * -Xloggc:/home/cloud/Desktop/gc.log
+ * -Xmx8m
+ * -Xms8m
+ * Created by cloud on 16-12-10.
+ *
+ */
+public class ReferenceCountingGC2 {
+    private Object intance = null;
+
+    private static final int _1M = 1024 * 1024;
+    private byte[] bigSize = new byte[_1M];
+
+
+    private void gc1(){
+        ReferenceCountingGC2 gc = new ReferenceCountingGC2();
+        ReferenceCountingGC2 gc1 = new ReferenceCountingGC2();
+        gc = null;
+        gc1 = null;
+        System.gc();
+    }
+
+    private void gc2(){
+        ReferenceCountingGC2 gc = new ReferenceCountingGC2();
+        ReferenceCountingGC2 gc1 = new ReferenceCountingGC2();
+        gc1.intance = gc;
+        gc.intance = gc1;
+        gc = null;
+        gc1 = null;
+        System.gc();
+    }
+
+    public static void main(String[] args) {
+        ReferenceCountingGC2 gc = new ReferenceCountingGC2();
+        ReferenceCountingGC2 gc1 = new ReferenceCountingGC2();
+        gc1.intance = gc;
+        gc.intance = gc1;
+        gc = null;
+        gc1 = null;
+        System.gc();
+    }
+}
